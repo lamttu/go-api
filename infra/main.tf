88e9ebfabfaff6b700a5d40fa3dba40e0b83ee24
Lam Tu <lam.tu@xero.com>
2021-04-06 18:06:19 +1000

    Add terraform for lambda

---
+++ infra/main.tf
@@ -0,0 +1,36 @@
+resource "aws_lambda_function" "get" {
+   function_name = "GoLambdaGetRequests"
+
+   # The bucket name as created earlier with "aws s3api create-bucket"
+   s3_bucket = "terraform-serverless-example"
+   s3_key    = "v1.0.0/example.zip"
+
+   # "main" is the filename within the zip file (main.js) and "handler"
+   # is the name of the property under which the handler function was
+   # exported in that file.
+   handler = "go-api"
+   runtime = "go1.x"
+
+   role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_iam_role" "lambda_exec" {
+   name = "serverless_example_lambda"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
