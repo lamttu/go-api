7fcaca95c4d6abdff261514fc095d3a89412a33b
Lam Tu <lam.tu@xero.com>
2021-04-06 18:10:33 +1000

    Add memory_size and timeout for lambda

--- infra/main.tf
+++ infra/main.tf
@@ -1,23 +1,25 @@
 resource "aws_lambda_function" "get" {
-   function_name = "GoLambdaGetRequests"
+  function_name = "GoLambdaGetRequests"
 
-   # The bucket name as created earlier with "aws s3api create-bucket"
-   s3_bucket = "terraform-serverless-example"
-   s3_key    = "v1.0.0/example.zip"
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "terraform-serverless-example"
+  s3_key    = "v1.0.0/example.zip"
 
-   # "main" is the filename within the zip file (main.js) and "handler"
-   # is the name of the property under which the handler function was
-   # exported in that file.
-   handler = "go-api"
-   runtime = "go1.x"
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler     = "go-api"
+  runtime     = "go1.x"
+  memory_size = 128
+  timeout     = 10
 
-   role = aws_iam_role.lambda_exec.arn
+  role = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_iam_role" "lambda_exec" {
-   name = "serverless_example_lambda"
+  name = "serverless_example_lambda"
 
-   assume_role_policy = <<EOF
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
