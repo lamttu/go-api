abb2aaf6d705ff86a7cb1c050014db7578a3f4dd
Lam Tu <lam.tu@xero.com>
2021-04-28 19:18:19 +1000

    Accommodate pathing in the repo

--- scripts.sh
+++ scripts.sh
@@ -4,11 +4,12 @@
 aws s3api create-bucket --bucket go-api-lambda --create-bucket-configuration LocationConstraint=ap-southeast-2 --profile personal
 
 # Build and zip package https://docs.aws.amazon.com/lambda/latest/dg/golang-package.html
-go build -o main main.go
+go build -o main src/main.go
 build-lambda-zip.exe -o go-api-get.zip main 
 
 # Copy zip file to the bucket
 aws s3 cp go-api-get.zip s3://go-api-lambda/go-api-get.zip 
 
 # Deploy terraform
-terraform apply
+terraform init infra/
+terraform apply infra/ -auto-approve
